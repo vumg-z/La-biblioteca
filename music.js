33540d442b3ec590925d9430656221e9b257c516
vumg <ab17100222@gmail.com>
2020-03-06 21:29:47 -0600

    added squares interaction

--- music.js
+++ music.js
@@ -1,23 +1,22 @@
-import data from "./data.js";
+import data from "./music_data.js";
 
-const picked = []
+const picked = [];
 
 const randomNumber = (max, min) => {
-  let number = Math.floor(Math.random() * (max - min) ) + min;
+  let number = Math.floor(Math.random() * (max - min)) + min;
 
-  if(!picked.includes(number)){ 
-    picked.push(number)
-    return number
-  }else{
-    return randomNumber(max,min)
+  if (!picked.includes(number)) {
+    picked.push(number);
+    return number;
+  } else {
+    return randomNumber(max, min);
   }
 };
 
-const music = () => {
+const getMusic = () => {
   const section = document.getElementById("section");
 
-  for (let i = 0; i<18; i++) {
-
+  for (let i = 0; i < 18; i++) {
     let node = document.createElement("iframe");
     node.setAttribute("src", `${data[randomNumber(data.length, 0)]}`);
     node.setAttribute("width", "300");
@@ -25,9 +24,42 @@ const music = () => {
     node.setAttribute("frameborder", "0");
     node.setAttribute("allowtransparency", "true");
     node.setAttribute("allow", "encrypted-media");
+    node.setAttribute("class", "hidden");
     section.appendChild(node);
   }
 };
 
-music();
+const setSquares = () => {
+  const section = document.getElementById("section");
 
+  for (let i = 0; i < 18; i++) {
+    let node = document.createElement("div");
+    node.setAttribute("class", "square");
+    section.appendChild(node);
+  }
+};
+
+const hideSquares = () => {
+  let squares = document.querySelectorAll(".square");
+
+  for (let i in squares) {
+    if (typeof squares[i] == "object")
+      squares[i].parentNode.removeChild(squares[i]);
+  }
+};
+
+const showMusic = () => {
+  let iframes = document.querySelectorAll(".hidden");
+  for (let i in iframes) {
+    iframes[i].classList = "show";
+  }
+};
+
+window.onload = () => {
+  // once all the data is loaded
+  hideSquares();
+  showMusic();
+};
+
+setSquares();
+getMusic();
